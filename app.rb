cfc15163ba0db8e6ef6ec9d6460fb82135563089
“sasacoJM” <78665068+Sasaco-JM@users.noreply.github.com>
2021-11-09 21:33:01 +0900

    connectionの呼び出し箇所を修正

--- app.rb
+++ app.rb
@@ -9,8 +9,7 @@ require 'dotenv/load'
 # ページ表示
 get '/' do
   @title = 'Top'
-  connection
-  @memos = select_all_memo(params)
+  @memos = select_all_memo
   @connection.finish
 
   erb :index
@@ -19,12 +18,12 @@ end
 # 追加ボタン
 get '/memos' do
   @title = 'new memo'
+
   erb :new
 end
 
 get '/memos/:id' do
   @title = 'show memo'
-  connection
   @memo = select_memo(params)
   @connection.finish
 
@@ -34,7 +33,6 @@ end
 # 編集ボタン
 get '/memos/:id/edit' do
   @title = 'edit memo'
-  connection
   @memo = select_memo(params)
   @connection.finish
 
@@ -44,7 +42,6 @@ end
 # データ操作
 # 保存ボタン
 post '/memos' do
-  connection
   insert_memo(params)
   @connection.finish
 
@@ -54,7 +51,6 @@ end
 
 # 変更ボタン
 patch '/memos/:id' do
-  connection
   update_memo(params)
   @connection.finish
 
@@ -64,7 +60,6 @@ end
 
 # 削除ボタン
 delete '/memos/:id' do
-  connection
   delete_memo(params)
   @connection.finish
 
@@ -75,23 +70,23 @@ end
 # メソッド
 
 def insert_memo(params)
-  @connection.exec('INSERT INTO memos(title,content) VALUES( $1,$2);', [params[:title], params[:content]])
+  connection.exec('INSERT INTO memos(title,content) VALUES( $1,$2);', [params[:title], params[:content]])
 end
 
 def update_memo(params)
-  @connection.exec('UPDATE memos SET title = $1,content = $2 WHERE id = $3;', [params[:title], params[:content], params[:id]])
+  connection.exec('UPDATE memos SET title = $1,content = $2 WHERE id = $3;', [params[:title], params[:content], params[:id]])
 end
 
 def delete_memo(params)
-  @connection.exec('DELETE FROM memos WHERE id = $1;', [params[:id]])
+  connection.exec('DELETE FROM memos WHERE id = $1;', [params[:id]])
 end
 
 def select_memo(params)
-  @connection.exec('SELECT * FROM memos WHERE id = $1;', [params[:id]])
+  connection.exec('SELECT * FROM memos WHERE id = $1;', [params[:id]])
 end
 
-def select_all_memo(_params)
-  @connection.exec('SELECT * FROM memos;')
+def select_all_memo
+  connection.exec('SELECT * FROM memos;')
 end
 
 def connection
